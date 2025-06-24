daf4d82783627a54cd71252c2d572b4cc4bdc763
Stephen Hurd <deuce@synchro.net>
2025-06-24 12:17:45 -0400

    Make the sources list data-driven as well.
    I shouldn't need to update the HTML when I update the data. I'm far
    too lazy and unorganized to do that.

--- form.js
+++ form.js
@@ -9,14 +9,8 @@
  */
 
 import wizard from "./wizard.js";
+import sources from "./sources.js";
 import { summary, generateSpellbook } from "./spellbook.js";
-
-function addOpt(opt, name) {
-	var newOption = document.createElement('option');
-	newOption.value = name;
-	newOption.innerHTML = name;
-	opt.appendChild(newOption);
-}
 
 const styleOpt = document.getElementById("style");
 const styleBiasOpt = document.getElementById("style-bias");
@@ -25,32 +19,23 @@ const schoolBiasOpt = document.getElementById("school-bias");
 const authorLevelOpt = document.getElementById("author");
 const output = document.getElementById("output");
 const generateButton = document.getElementById("generate");
-const sources = document.getElementsByName("sources");
+const sourceDiv = document.getElementById("sources");
 
-for (const style of summary.styles) {
-	addOpt(styleOpt, style);
+function addOpt(opt, name) {
+	var newOption = document.createElement('option');
+	newOption.value = name;
+	newOption.innerHTML = name;
+	opt.appendChild(newOption);
 }
 
-for (const school of summary.schools) {
-	addOpt(schoolOpt, school);
-}
-
-styleBiasOpt.innerHTML = '';
-for (let i = 0; i < 100; i += 5) {
-	addOpt(styleBiasOpt, i);
-	addOpt(schoolBiasOpt, i);
-}
-
-authorLevelOpt.innerHTML = '';
-for (const level in wizard) {
-	if (Object.hasOwn(wizard, level)) {
-		addOpt(authorLevelOpt, level);
-	}
+function addSource(source, label, def) {
+	sourceDiv.innerHTML += '<label><input type="checkbox" name="sources" value="' + source + '"' + (def ? ' checked' : '') + "> " + label + "</label> ";
 }
 
 function readSources() {
+	const se = document.getElementsByName("sources");
 	let ret = [];
-	for (const source of sources) {
+	for (const source of se) {
 		if (source.checked) {
 			ret.push(source.value);
 		}
@@ -75,6 +60,33 @@ function generate() {
 	catch (e) {
 		alert(e.message);
 	}
+}
+
+// Fill out the form...
+
+for (const style of summary.styles) {
+	addOpt(styleOpt, style);
+}
+
+for (const school of summary.schools) {
+	addOpt(schoolOpt, school);
+}
+
+styleBiasOpt.innerHTML = '';
+for (let i = 0; i < 100; i += 5) {
+	addOpt(styleBiasOpt, i);
+	addOpt(schoolBiasOpt, i);
+}
+
+authorLevelOpt.innerHTML = '';
+for (const level in wizard) {
+	if (Object.hasOwn(wizard, level)) {
+		addOpt(authorLevelOpt, level);
+	}
+}
+
+for (const source of sources) {
+	addSource(source.source, source.name, source.enabled);
 }
 
 generateButton.addEventListener('click', ()=>{generate();});
---
+++ sources.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		'source': 'DM1',
+		'name': 'Deep Magic Vol. 1',
+		'enabled': true
+	},
+	{
+		'source': 'DM2',
+		'name': 'Deep Magic Vol. 2',
+		'enabled': true
+	},
+	{
+		'source': 'PG',
+		'name': "ToV Player's Guide",
+		'enabled': true
+	},
+	{
+		'source': 'BFRD',
+		'name': 'Black Flag Reference Document',
+		'enabled': false
+	},
+	{
+		'source': 'TOM',
+		'name': 'The Old Margreve',
+		'enabled': true
+	},
+	{
+		'source': 'LW',
+		'name': 'Labyrinth Worldbook',
+		'enabled': true
+	},
+];
